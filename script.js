01843f6bf79c6d18245a1449cc1892db53eda4e3
Lucia Vu <luciavu6@outlook.com>
2024-10-03 02:14:32 +1000

    Move event listeners to JS

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
-function resetBoard(){
-    const grid = document.getElementById("grid");
-    for (let i = 0; i < 9; i++) {
-        // Create cell 
-        const cell = document.createElement("div");
-        cell.className = "cell";
-        cell.setAttribute("data-index", i);
-        cell.textContent = "";
-        // Assign onclick handler
-        cell.onclick = function() {
-            markCell(i);
-        };
-        grid.appendChild(cell);
-    }
-};
+document.addEventListener('DOMContentLoaded', () => {
+    
+    function resetBoard(){
+        const grid = document.getElementById("grid");
+        // Clear grid
+        grid.textContent= "";
+        
+        for (let i = 0; i < 9; i++) {
+            // Create cell 
+            const cell = document.createElement("div");
+            cell.className = "cell";
+            cell.setAttribute("data-index", i);
+            cell.textContent = "";
+            // Assign onclick handler
+            cell.onclick = function() {
+                markCell(i);
+            };
+            grid.appendChild(cell);
+        }
+    };
 
-function newGame() {
-    resetBoard();
-};
-function updateName(name){};
-function markCell(cell){};
-function processMove(){};
-function updateGameStatus(){};
-function endGame(winner){};
+    function newGame() {
+        resetBoard();
+    };
+    function updateName(name){};
+    function markCell(cell){};
+    function processMove(gameArray){};
+    function updateGameStatus(){};
+    function endGame(winner){};
 
-newGame();
+    newGame();
+    newGame();
+
+});
